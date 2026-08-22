c2ae46ce9c793119631dc04219fedc0a6c505971
NNYYOONNIIOO <wubidaxiang@163.com>
2026-08-22 10:07:59 +0800

    fix: connect rings to Botania mana items

--- src/main/java/mythicbotany/item/ItemMythicRing.java
+++ src/main/java/mythicbotany/item/ItemMythicRing.java
@@ -7,11 +7,13 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.init.MobEffects;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import vazkii.botania.api.mana.IManaItem;
 
-public class ItemMythicRing extends Item implements IBauble {
+public class ItemMythicRing extends Item implements IBauble, IManaItem {
     public enum Effect {
         MANA, AURA, FIRE, ICE, ANDWARI
     }
@@ -35,7 +37,10 @@ public class ItemMythicRing extends Item implements IBauble {
         }
         switch (effect) {
             case MANA:
-                player.addPotionEffect(new PotionEffect(MobEffects.REGENERATION, 40, 0));
+                if (getMana(stack) >= 1000 && player.getHealth() < player.getMaxHealth()) {
+                    player.heal(1.0F);
+                    addMana(stack, -1000);
+                }
                 break;
             case AURA:
                 player.addPotionEffect(new PotionEffect(MobEffects.SPEED, 40, 0));
@@ -74,5 +79,47 @@ public class ItemMythicRing extends Item implements IBauble {
     @Override
     public boolean canUnequip(ItemStack itemstack, EntityLivingBase player) {
         return true;
+    }
+
+    @Override
+    public int getMana(ItemStack stack) {
+        NBTTagCompound tag = stack.getTagCompound();
+        return tag == null ? 0 : Math.max(0, Math.min(getMaxMana(stack), tag.getInteger("Mana")));
+    }
+
+    @Override
+    public int getMaxMana(ItemStack stack) {
+        return 2000000;
+    }
+
+    @Override
+    public void addMana(ItemStack stack, int amount) {
+        int next = Math.max(0, Math.min(getMaxMana(stack), getMana(stack) + amount));
+        stack.setTagInfo("Mana", new net.minecraft.nbt.NBTTagInt(next));
+    }
+
+    @Override
+    public boolean canReceiveManaFromPool(ItemStack stack, TileEntity pool) {
+        return getMana(stack) < getMaxMana(stack);
+    }
+
+    @Override
+    public boolean canReceiveManaFromItem(ItemStack stack, ItemStack otherStack) {
+        return getMana(stack) < getMaxMana(stack);
+    }
+
+    @Override
+    public boolean canExportManaToPool(ItemStack stack, TileEntity pool) {
+        return getMana(stack) > 0;
+    }
+
+    @Override
+    public boolean canExportManaToItem(ItemStack stack, ItemStack otherStack) {
+        return getMana(stack) > 0;
+    }
+
+    @Override
+    public boolean isNoExport(ItemStack stack) {
+        return false;
     }
 }
