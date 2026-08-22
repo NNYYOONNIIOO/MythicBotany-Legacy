cbb4bef0812286875b08dfb014f94144510e27dd
NNYYOONNIIOO <wubidaxiang@163.com>
2026-08-22 15:43:04 +0800

    fix: complete alfsteel mana burst lens behavior

--- src/main/java/mythicbotany/item/ItemAlfsteelSword.java
+++ src/main/java/mythicbotany/item/ItemAlfsteelSword.java
@@ -1,5 +1,28 @@
 package mythicbotany.item;
 
+import net.minecraft.entity.projectile.EntityThrowable;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import vazkii.botania.api.BotaniaAPI;
+import vazkii.botania.api.internal.IManaBurst;
+import vazkii.botania.api.mana.BurstProperties;
+import vazkii.botania.api.mana.ILensEffect;
+import vazkii.botania.common.core.handler.ModSounds;
+import vazkii.botania.common.item.ModItems;
+import vazkii.botania.common.item.equipment.tool.ToolCommons;
+import vazkii.botania.common.item.equipment.tool.manasteel.ItemManasteelSword;
+import vazkii.botania.common.lib.LibItemNames;
+import vazkii.botania.common.network.PacketHandler;
+import vazkii.botania.common.network.PacketLeftClick;
+import javax.annotation.Nonnull;
+import java.util.List;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
@@ -15,6 +38,8 @@ import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
+import vazkii.botania.api.mana.ILensEffect;
+import vazkii.botania.api.internal.IManaBurst;
 import vazkii.botania.api.mana.IManaItem;
 import vazkii.botania.api.mana.IManaTooltipDisplay;
 import vazkii.botania.api.mana.ManaItemHandler;
@@ -22,10 +47,12 @@ import vazkii.botania.common.core.helper.ItemNBTHelper;
 import vazkii.botania.common.entity.EntityManaBurst;
 
 /** Alfsteel sword with a native Botania 1.12 mana-burst beam. */
-public class ItemAlfsteelSword extends ItemSword implements IManaItem, IManaTooltipDisplay {
+public class ItemAlfsteelSword extends ItemSword implements IManaItem, IManaTooltipDisplay, ILensEffect {
     private static final String TAG_MANA = "mana";
     private static final int MAX_MANA = 4000000;
     private static final int BURST_MANA = 1000;
+    private static final int MANA_PER_DAMAGE = BURST_MANA;
+    private static final String TAG_ATTACKER_USERNAME = "attackerUsername";
 
     public ItemAlfsteelSword(Item.ToolMaterial material) {
         super(material);
@@ -64,6 +91,9 @@ public class ItemAlfsteelSword extends ItemSword implements IManaItem, IManaTool
         burst.setMinManaLoss(40);
         burst.setManaLossPerTick(40.0F);
         burst.setGravity(0.0F);
+        ItemStack lens = player.getHeldItem(hand).copy();
+        ItemNBTHelper.setString(lens, TAG_ATTACKER_USERNAME, player.getName());
+        burst.setSourceLens(lens);
         return burst;
     }
 
@@ -126,4 +156,55 @@ public class ItemAlfsteelSword extends ItemSword implements IManaItem, IManaTool
     public int getRGBDurabilityForDisplay(ItemStack stack) {
         return MathHelper.hsvToRGB(getManaFractionForDisplay(stack) / 3.0F, 1.0F, 1.0F);
     }
+
+    @Override
+    public boolean doParticles(IManaBurst burst, ItemStack stack) {
+        return true;
+    }
+
+    @Override
+    public void apply(ItemStack stack, BurstProperties props) {
+        // The sword configures its burst directly when it is fired.
+    }
+
+    @Override
+    public boolean collideBurst(IManaBurst burst, RayTraceResult pos, boolean isManaBlock, boolean dead, ItemStack stack) {
+        return dead;
+    }
+
+    @Override
+    public void updateBurst(IManaBurst burst, ItemStack stack) {
+            EntityThrowable entity = (EntityThrowable) burst;
+            AxisAlignedBB axis = new AxisAlignedBB(entity.posX, entity.posY, entity.posZ, entity.lastTickPosX, entity.lastTickPosY, entity.lastTickPosZ).grow(1);
+            List<EntityLivingBase> entities = entity.world.getEntitiesWithinAABB(EntityLivingBase.class, axis);
+            String attacker = ItemNBTHelper.getString(burst.getSourceLens(), TAG_ATTACKER_USERNAME, "");
+
+            for(EntityLivingBase living : entities) {
+                if(living instanceof EntityPlayer && (living.getName().equals(attacker) || FMLCommonHandler.instance().getMinecraftServerInstance() != null && !FMLCommonHandler.instance().getMinecraftServerInstance().isPVPEnabled()))
+                    continue;
+
+                if(living.hurtTime == 0) {
+                    int cost = MANA_PER_DAMAGE / 3;
+                    int mana = burst.getMana();
+                    if(mana >= cost) {
+                        burst.setMana(mana - cost);
+                        float damage = 4F + getAttackDamage();
+                        if(!burst.isFake() && !entity.world.isRemote) {
+                            EntityPlayer player = living.world.getPlayerEntityByName(attacker);
+                            living.attackEntityFrom(player == null ? DamageSource.MAGIC : DamageSource.causePlayerDamage(player), damage);
+                            entity.setDead();
+                            break;
+                        }
+                    }
+                }
+            }
+        }
 }
+
+
+
+
+
+
+
+
