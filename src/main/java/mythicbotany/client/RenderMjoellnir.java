910eb1878b2e471af5ae524cefdcb09d6de7d894
NNYYOONNIIOO <wubidaxiang@163.com>
2026-08-24 20:17:13 +0800

    fix: render Mjoellnir from baked block model

--- src/main/java/mythicbotany/client/RenderMjoellnir.java
+++ src/main/java/mythicbotany/client/RenderMjoellnir.java
@@ -1,24 +1,54 @@
 package mythicbotany.client;
 
 import mythicbotany.entity.EntityMjoellnir;
+import mythicbotany.registry.ModBlocks;
 import mythicbotany.registry.ModItems;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.RenderItem;
+import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-import vazkii.botania.client.render.entity.RenderSnowballStack;
 
 @SideOnly(Side.CLIENT)
-public final class RenderMjoellnir extends RenderSnowballStack<EntityMjoellnir> {
+public final class RenderMjoellnir extends Render<EntityMjoellnir> {
+    private final RenderItem renderItem;
+
     public RenderMjoellnir(RenderManager manager) {
-        super(manager, ModItems.mjoellnir, Minecraft.getMinecraft().getRenderItem(),
-                EntityMjoellnir::getItem);
+        super(manager);
+        renderItem = Minecraft.getMinecraft().getRenderItem();
+        shadowSize = 0.0F;
     }
 
     @Override
-    public ItemStack getStackToRender(EntityMjoellnir entity) {
+    public void doRender(EntityMjoellnir entity, double x, double y, double z,
+                         float entityYaw, float partialTicks) {
         ItemStack stack = entity.getItem();
-        return stack.isEmpty() ? new ItemStack(ModItems.mjoellnir) : stack;
+        if (stack.isEmpty()) {
+            stack = new ItemStack(ModItems.mjoellnir);
+        }
+
+        IBakedModel model = Minecraft.getMinecraft().getBlockRendererDispatcher()
+                .getModelForState(ModBlocks.mjoellnir.getDefaultState());
+        GlStateManager.pushMatrix();
+        try {
+            GlStateManager.translate(x, y, z);
+            GlStateManager.enableRescaleNormal();
+            GlStateManager.scale(0.5F, 0.5F, 0.5F);
+            GlStateManager.rotate(180.0F - entityYaw, 0.0F, 1.0F, 0.0F);
+            renderItem.renderItem(stack, model);
+        } finally {
+            GlStateManager.disableRescaleNormal();
+            GlStateManager.popMatrix();
+        }
+    }
+
+    @Override
+    protected ResourceLocation getEntityTexture(EntityMjoellnir entity) {
+        return null;
     }
 }
