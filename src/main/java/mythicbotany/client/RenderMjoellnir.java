6f2924e743767e7d55298650576f1791fc409632
NNYYOONNIIOO <wubidaxiang@163.com>
2026-08-24 17:41:20 +0800

    fix: render Mjoellnir entities with item model

--- src/main/java/mythicbotany/client/RenderMjoellnir.java
+++ src/main/java/mythicbotany/client/RenderMjoellnir.java
@@ -1,43 +1,21 @@
 package mythicbotany.client;
 
 import mythicbotany.entity.EntityMjoellnir;
-import mythicbotany.registry.ModBlocks;
+import mythicbotany.registry.ModItems;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.BlockRendererDispatcher;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
-import net.minecraft.client.renderer.texture.TextureMap;
-import net.minecraft.util.ResourceLocation;
+import vazkii.botania.client.render.entity.RenderSnowballStack;
+
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+/** Renders the thrown hammer using the registered Mjoellnir item model. */
 @SideOnly(Side.CLIENT)
-public final class RenderMjoellnir extends Render<EntityMjoellnir> {
+public final class RenderMjoellnir extends RenderSnowballStack<EntityMjoellnir> {
     public RenderMjoellnir(RenderManager manager) {
-        super(manager);
-    }
-
-    @Override
-    public void doRender(EntityMjoellnir entity, double x, double y, double z,
-                         float entityYaw, float partialTicks) {
-        GlStateManager.pushMatrix();
-        bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
-        GlStateManager.translate((float) x - 0.5F, (float) y - 0.5F, (float) z - 0.5F);
-        float yaw = entity.prevRotationYaw
-                + (entity.rotationYaw - entity.prevRotationYaw) * partialTicks;
-        float pitch = entity.prevRotationPitch
-                + (entity.rotationPitch - entity.prevRotationPitch) * partialTicks;
-        GlStateManager.rotate(yaw - 90.0F, 0.0F, 1.0F, 0.0F);
-        GlStateManager.rotate(pitch, 0.0F, 0.0F, 1.0F);
-        GlStateManager.rotate(90.0F, 0.0F, 0.0F, -1.0F);
-        BlockRendererDispatcher dispatcher = Minecraft.getMinecraft().getBlockRendererDispatcher();
-        dispatcher.renderBlockBrightness(ModBlocks.mjoellnir.getDefaultState(), 1.0F);
-        GlStateManager.popMatrix();
-    }
-
-    @Override
-    protected ResourceLocation getEntityTexture(EntityMjoellnir entity) {
-        return null;
+        super(manager, ModItems.mjoellnir, Minecraft.getMinecraft().getRenderItem(),
+                entity -> entity.getItem().isEmpty()
+                        ? new net.minecraft.item.ItemStack(ModItems.mjoellnir)
+                        : entity.getItem());
     }
 }
--- src/main/java/mythicbotany/client/RenderMjoellnirPlaced.java
+++ src/main/java/mythicbotany/client/RenderMjoellnirPlaced.java
@@ -1,43 +1,21 @@
 package mythicbotany.client;
 
 import mythicbotany.entity.EntityMjoellnirPlaced;
-import mythicbotany.registry.ModBlocks;
+import mythicbotany.registry.ModItems;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.BlockRendererDispatcher;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
-import net.minecraft.client.renderer.texture.TextureMap;
-import net.minecraft.util.ResourceLocation;
+import vazkii.botania.client.render.entity.RenderSnowballStack;
+
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+/** Renders the fallback hammer using the registered Mjoellnir item model. */
 @SideOnly(Side.CLIENT)
-public final class RenderMjoellnirPlaced extends Render<EntityMjoellnirPlaced> {
+public final class RenderMjoellnirPlaced extends RenderSnowballStack<EntityMjoellnirPlaced> {
     public RenderMjoellnirPlaced(RenderManager manager) {
-        super(manager);
-    }
-
-    @Override
-    public void doRender(EntityMjoellnirPlaced entity, double x, double y, double z,
-                         float entityYaw, float partialTicks) {
-        GlStateManager.pushMatrix();
-        bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
-        GlStateManager.translate((float) x - 0.5F, (float) y - 0.5F, (float) z - 0.5F);
-        float yaw = entity.prevRotationYaw
-                + (entity.rotationYaw - entity.prevRotationYaw) * partialTicks;
-        float pitch = entity.prevRotationPitch
-                + (entity.rotationPitch - entity.prevRotationPitch) * partialTicks;
-        GlStateManager.rotate(yaw - 90.0F, 0.0F, 1.0F, 0.0F);
-        GlStateManager.rotate(pitch, 0.0F, 0.0F, 1.0F);
-        GlStateManager.rotate(90.0F, 0.0F, 0.0F, -1.0F);
-        BlockRendererDispatcher dispatcher = Minecraft.getMinecraft().getBlockRendererDispatcher();
-        dispatcher.renderBlockBrightness(ModBlocks.mjoellnir.getDefaultState(), 1.0F);
-        GlStateManager.popMatrix();
-    }
-
-    @Override
-    protected ResourceLocation getEntityTexture(EntityMjoellnirPlaced entity) {
-        return null;
+        super(manager, ModItems.mjoellnir, Minecraft.getMinecraft().getRenderItem(),
+                entity -> entity.getItem().isEmpty()
+                        ? new net.minecraft.item.ItemStack(ModItems.mjoellnir)
+                        : entity.getItem());
     }
 }
