d4bacff381f21c9c90f21231f665462969b71f9f
NNYYOONNIIOO <wubidaxiang@163.com>
2026-08-24 21:35:38 +0800

    fix: aim Mjoellnir handle at thrower

--- src/main/java/mythicbotany/client/RenderMjoellnir.java
+++ src/main/java/mythicbotany/client/RenderMjoellnir.java
@@ -3,6 +3,7 @@ package mythicbotany.client;
 import mythicbotany.entity.EntityMjoellnir;
 import mythicbotany.registry.ModBlocks;
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.client.renderer.BlockRendererDispatcher;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.Render;
@@ -32,10 +33,39 @@ public final class RenderMjoellnir extends Render<EntityMjoellnir> {
                 + (entity.rotationPitch - entity.prevRotationPitch) * partialTicks;
         GlStateManager.pushMatrix();
         try {
-            GlStateManager.translate(x, y, z);
-            GlStateManager.rotate(yaw, 0.0F, 1.0F, 0.0F);
-            GlStateManager.rotate(-MODEL_Y_ROTATION, 1.0F, 0.0F, 0.0F);
-            GlStateManager.rotate(-pitch, 1.0F, 0.0F, 0.0F);
+        GlStateManager.translate(x, y + 1.0D, z);
+            EntityLivingBase thrower = entity.getThrower();
+            if (thrower != null) {
+                double entityX = entity.prevPosX + (entity.posX - entity.prevPosX) * partialTicks;
+                double entityY = entity.prevPosY + (entity.posY - entity.prevPosY) * partialTicks;
+                double entityZ = entity.prevPosZ + (entity.posZ - entity.prevPosZ) * partialTicks;
+                double targetX = thrower.prevPosX + (thrower.posX - thrower.prevPosX) * partialTicks;
+                double targetY = thrower.prevPosY + (thrower.posY - thrower.prevPosY) * partialTicks + thrower.getEyeHeight() * 0.5D;
+                double targetZ = thrower.prevPosZ + (thrower.posZ - thrower.prevPosZ) * partialTicks;
+                double directionX = targetX - entityX;
+                double directionY = targetY - entityY;
+                double directionZ = targetZ - entityZ;
+                double length = Math.sqrt(directionX * directionX + directionY * directionY + directionZ * directionZ);
+                if (length > 1.0E-6D) {
+                    directionX /= length;
+                    directionY /= length;
+                    directionZ /= length;
+                    double axisX = directionZ;
+                    double axisZ = -directionX;
+                    double axisLength = Math.sqrt(axisX * axisX + axisZ * axisZ);
+                    float angle = (float) (Math.acos(Math.max(-1.0D, Math.min(1.0D, directionY))) * 180.0D / Math.PI);
+                    if (axisLength > 1.0E-6D) {
+                        GlStateManager.rotate(angle, (float) (axisX / axisLength), 0.0F, (float) (axisZ / axisLength));
+                    } else if (directionY < 0.0D) {
+                        GlStateManager.rotate(180.0F, 1.0F, 0.0F, 0.0F);
+                    }
+                    GlStateManager.rotate(MODEL_Y_ROTATION, 0.0F, 1.0F, 0.0F);
+                }
+            } else {
+                GlStateManager.rotate(yaw, 0.0F, 1.0F, 0.0F);
+                GlStateManager.rotate(-MODEL_Y_ROTATION, 1.0F, 0.0F, 0.0F);
+                GlStateManager.rotate(-pitch, 1.0F, 0.0F, 0.0F);
+            }
             GlStateManager.translate(-0.5D, -0.5D, -0.5D);
             bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
             dispatcher.renderBlockBrightness(ModBlocks.mjoellnir.getDefaultState(), 1.0F);
