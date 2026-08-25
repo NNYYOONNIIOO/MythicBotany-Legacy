700c6d0f8b2df482d591c42d498a49b1393b811e
NNYYOONNIIOO <wubidaxiang@163.com>
2026-08-25 11:32:04 +0800

    fix: localize Alfsteel pylon HUD

--- src/main/java/mythicbotany/block/BlockAlfsteelPylon.java
+++ src/main/java/mythicbotany/block/BlockAlfsteelPylon.java
@@ -61,17 +61,8 @@ public class BlockAlfsteelPylon extends BlockContainer implements IWandable, IWa
         TileEntity tile = world.getTileEntity(pos);
         if (tile instanceof TileAlfsteelPylon) {
             TileAlfsteelPylon pylon = (TileAlfsteelPylon) tile;
-            String key = "tile.mythicbotany:alfsteel_pylon.name";
-            String name = I18n.format(key);
-            if (name.equals(key)) {
-                String legacyKey = "tile.mythicbotany.alfsteel_pylon.name";
-                name = I18n.format(legacyKey);
-                if (name.equals(legacyKey)) {
-                    name = I18n.format("block.mythicbotany.alfsteel_pylon");
-                }
-            }
             HUDHandler.drawSimpleManaHUD(0xFFAA00, pylon.getCurrentMana(),
-                    pylon.getMaxMana(), name, res);
+                    pylon.getMaxMana(), I18n.format("block.mythicbotany.alfsteel_pylon"), res);
         }
     }
 
