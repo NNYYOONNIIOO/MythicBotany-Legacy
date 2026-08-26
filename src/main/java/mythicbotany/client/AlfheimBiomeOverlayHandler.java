12bfe7f327a5da9ac2729cb1fc643baba589424b
NNYYOONNIIOO <wubidaxiang@163.com>
2026-08-26 16:55:01 +0800

    Ensure Alfheim biome names are localized

--- src/main/java/mythicbotany/client/AlfheimBiomeOverlayHandler.java
+++ src/main/java/mythicbotany/client/AlfheimBiomeOverlayHandler.java
@@ -3,20 +3,18 @@ package mythicbotany.client;
 import mythicbotany.MythicBotany;
 import net.minecraft.client.resources.I18n;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.relauncher.Side;
 
 import java.util.List;
 
 /** Localizes Alfheim biome registry ids shown by the F3 overlay. */
-@Mod.EventBusSubscriber(value = Side.CLIENT, modid = MythicBotany.MODID)
 public final class AlfheimBiomeOverlayHandler {
     private static final String NAMESPACE = MythicBotany.MODID + ".";
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
-    public static void localizeBiomeLine(RenderGameOverlayEvent.Text event) {
+    public void localizeBiomeLine(RenderGameOverlayEvent.Text event) {
+        AlfheimBiomeLocalization.apply();
         localize(event.getLeft());
         localize(event.getRight());
     }
